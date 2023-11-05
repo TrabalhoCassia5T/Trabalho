125eae97c346a7b1183acc3798a20171ddc5b608
TrabalhoCassia5T <kaueunoeste@gmail.com>
2023-11-05 17:47:59 -0300

    tela de busca + integração com o backend

--- UnoLibrary/Back-End/src/main/java/com/example/UnoLibrary/Controllers/ClientController.java
+++ UnoLibrary/Back-End/src/main/java/com/example/UnoLibrary/Controllers/ClientController.java
@@ -19,21 +19,24 @@ public class ClientController {
     @PostMapping(value = "/cadastro-cliente")
     public ResponseEntity<Object> saveClient(@RequestParam("nome") String nome,
                                              @RequestParam("sobrenome") String sobrenome,
-                                             @RequestParam("endereco") String endereco,
-                                             @RequestParam("cidade") String cidade,
-                                             @RequestParam("telefone") String telefone,
                                              @RequestParam("cpf") String cpf,
                                              @RequestParam("dataNasc") String dataNasc,
-                                             @RequestParam("email") String email)
+                                             @RequestParam("email") String email,
+                                             @RequestParam("telefone") String telefone,
+                                             @RequestParam("endereco") String endereco,
+                                             @RequestParam("uf") String estado,
+                                             @RequestParam("cep") String cep,
+                                             @RequestParam("cidade") String cidade)
     {
-        ClientRequestDTO dados = new ClientRequestDTO(nome, sobrenome, telefone, cpf, endereco, cidade, dataNasc, email);
+        System.out.println(cpf);
+        ClientRequestDTO dados = new ClientRequestDTO(nome, sobrenome, telefone, cpf, endereco, cidade, dataNasc, email, cep, estado);
         Client data = new Client(dados);
         repository.save(data);
         return ResponseEntity.ok().body("ok");
     }
 
-    @GetMapping(value = "/busca-cliente-cpf/{cpf}")
-    public ResponseEntity<Object> findByCpf(@PathVariable("cpf") String cpf)
+    @GetMapping(value = "/busca-cliente-cpf")
+    public ResponseEntity<Object> findByCpf(@RequestParam("cpf") String cpf)
     {
         return ResponseEntity.ok(repository.findByCpf(cpf));
     }
@@ -44,8 +47,8 @@ public class ClientController {
         return ResponseEntity.ok(repository.findAll());
     }
 
-    @GetMapping(value = "/busca-cliente-nome/{nome}")
-    public ResponseEntity<Object> findById(@PathVariable("nome") String nome)
+    @GetMapping(value = "/busca-cliente-nome")
+    public ResponseEntity<Object> findById(@RequestParam("nome") String nome)
     {
         return ResponseEntity.ok(repository.findByNome(nome));
     }
