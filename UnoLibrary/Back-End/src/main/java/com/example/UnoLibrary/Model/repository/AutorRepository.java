cd32397122c1fb2b42877cddb2d3a418fb162d7c
Leonardo Gomes Anholetto <leogomesanholetto@gmail.com>
2023-11-12 01:28:08 -0300

    Controlles Editora e Autor

---
+++ UnoLibrary/Back-End/src/main/java/com/example/UnoLibrary/Model/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.example.UnoLibrary.Model.repository;
+
+
+import com.example.UnoLibrary.Model.entity.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+}
