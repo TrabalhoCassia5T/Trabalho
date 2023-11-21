c9d8554410b7c0979872a45597039c71e964ce95
Jessica Perdomo <j.perdomoalves@gmail.com>
2023-11-21 16:47:01 -0300

    Novos repositórios

---
+++ UnoLibrary/Back-End/src/main/java/com/example/UnoLibrary/Model/repository/PedidoRepository.java
@@ -0,0 +1,7 @@
+package com.example.UnoLibrary.Model.repository;
+
+import com.example.UnoLibrary.Model.entity.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
