f9e56a89724a2c46ffe3451fdb068de17c226030
Jessica Perdomo <j.perdomoalves@gmail.com>
2023-11-22 21:11:17 -0300

    Subindo script certo

---
+++ UnoLibrary/Front-End/scripts/script.js
@@ -0,0 +1,40 @@
+function gravar() {
+  if (document.getElementById("login").value !== "" && document.getElementById("senha").value !== "") {
+    event.preventDefault();
+    const dados = new FormData(document.getElementById("formlogin"));
+    fetch("http://localhost:8080/api/verificar-login", { method: 'post', body: dados })
+      .then(response => response.text())
+      .then(mens => {
+        if (mens === "Não existe Cadastro" ) 
+        {
+          alert("Não possui cadastro");
+          window.location.href = "empresa/telaCadEmpresa.html" // Exibe o alerta com a mensagem
+        } 
+        else 
+        { 
+          window.location.href = "home/index.html"
+        }
+      })
+      .catch(error => document.getElementById("feedback").innerHTML = error);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
